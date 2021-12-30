714c35a1d39acbb58d3f0db6e7018194b2729fa7
JinKyung Kim <kimradium@naver.com>
2021-12-30 23:00:17 +0900

    #9 재배포

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "Components/scss/Loading.scss";
-import { ClapSpinner, SwapSpinner } from "react-spinners-kit";
+import { SwapSpinner } from "react-spinners-kit";
 
 const Loading = () => {
   return (
